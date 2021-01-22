11baff793d322e0f9ffa7f2649c0beda66e0811d
Mikhail Yudin <fagci.nsk@gmail.com>
2021-01-23 05:13:36 +0700

    checkpoint

--- yex/yex.py
+++ yex/yex.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 from jinja2.environment import Environment, Template
 from jinja2.loaders import FileSystemLoader
 import yaml
@@ -5,22 +6,33 @@ from yaml.loader import FullLoader
 
 
 class Yex:
-    def __init__(self, templates_path):
+    def __init__(self, t: str):
         """Init with yaml templates folder"""
-        self._env = Environment(loader=FileSystemLoader(templates_path))
+        # is template string
+        if '{' in t:
+            self._t = Template(t)
+        # is file
+        else:
+            self._env = Environment(loader=FileSystemLoader(t))
 
-    def render_file(self, file, *args, **context):
+    def render(self, *args, **context):
         """Render yaml with context"""
-        template = self._env.get_template(file)
-        yaml_text = self._render(template, *args, **context)
-        return self._load(yaml_text)
+        return self._load(self.render_text(*args, **context))
 
-    @classmethod
-    def render_text(cls, text, *args, **context):
-        """Render pure yaml text template with context"""
-        template = Template(text)
-        yaml_text = cls._render(template, *args, **context)
-        return cls._load(yaml_text)
+    def render_text(self, *args, **context):
+        if self._t:
+            """Render pure yaml text template with context"""
+            return self._render(self._t, *args, **context)
+
+        if self._env:
+            template = self._env.get_template(args[0])
+            return self._render(template, *args, **context)
+
+        return ''
+
+    def generate(self, data: Iterable):
+        for d in data:
+            yield self.render(**d)
 
     @staticmethod
     def _render(template: Template, *args, **kwargs):
@@ -29,3 +41,42 @@ class Yex:
     @staticmethod
     def _load(text: str):
         return yaml.load(text, Loader=FullLoader)
+
+    def __lt__(self, b):
+        return self.render(self._t, b)
+
+    def __call__(self, *args, **kwargs):
+        return self.render(*args, **kwargs)
+
+
+if __name__ == "__main__":
+    template = """
+    meta:
+    {% for k, v in meta.items() %}
+      {{ k }}: >-
+        {{ v }}
+    {% endfor %}
+    """
+    data = [
+        {
+            'meta': {
+                'title': 'Hello, world 1!',
+                'content': """<h1>Wellcome to our site!</h1>
+            <p>Glad to see you!</p>
+            """
+            }
+        },
+        {
+            'meta': {
+                'title': 'Hello, world 2!',
+                'content': """<h1>Wellcome to our site!</h1>
+            <p>Glad to see you!</p>
+            """
+            }
+        },
+    ]
+    content_page_template = """<title>{{title}}</title>
+    <div>{{content}}</div>"""
+    content_page = Yex(content_page_template)
+    for cfg in Yex(template).generate(data):
+        print(content_page.render_text(cfg))
