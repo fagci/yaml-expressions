b140f8f7947cc930bf0de8bc3ec30753ff2bb3e6
Mikhail Yudin <fagci.nsk@gmail.com>
2021-01-23 13:06:48 +0700

    checkpoint

--- yex/yex.py
+++ yex/yex.py
@@ -17,11 +17,12 @@ class Yex:
 
     def render(self, *args, **context):
         """Render yaml with context"""
-        return self._load(self.render_text(*args, **context))
+        return self._load(self.render_content(*args, **context))
 
-    def render_text(self, *args, **context):
+    def render_content(self, *args, **context):
         if self._t:
             """Render pure yaml text template with context"""
+            print('[*] render content', self._t, *args)
             return self._render(self._t, *args, **context)
 
         if self._env:
@@ -32,7 +33,10 @@ class Yex:
 
     def generate(self, data: Iterable):
         for d in data:
-            yield self.render(**d)
+            print('[*] render with', d)
+            r = self.render(d)
+            print('[.] got', r)
+            yield r
 
     @staticmethod
     def _render(template: Template, *args, **kwargs):
@@ -43,7 +47,7 @@ class Yex:
         return yaml.load(text, Loader=FullLoader)
 
     def __lt__(self, b):
-        return self.render(self._t, b)
+        return self.render(self._t, **b)
 
     def __call__(self, *args, **kwargs):
         return self.render(*args, **kwargs)
@@ -56,11 +60,18 @@ if __name__ == "__main__":
       {{ k }}: >-
         {{ v }}
     {% endfor %}
+    data:
+    {% for k, v in data.items() %}
+      {{ k }}: >-
+        {{ v }}
+    {% endfor %}
     """
     data = [
         {
             'meta': {
                 'title': 'Hello, world 1!',
+            },
+            'data': {
                 'content': """<h1>Wellcome to our site!</h1>
             <p>Glad to see you!</p>
             """
@@ -69,14 +80,17 @@ if __name__ == "__main__":
         {
             'meta': {
                 'title': 'Hello, world 2!',
+            },
+            'data': {
                 'content': """<h1>Wellcome to our site!</h1>
             <p>Glad to see you!</p>
             """
             }
         },
     ]
-    content_page_template = """<title>{{title}}</title>
-    <div>{{content}}</div>"""
+    content_page_template = """<title>{{meta.title}}</title>
+    <div>{{data.content}}</div>"""
     content_page = Yex(content_page_template)
     for cfg in Yex(template).generate(data):
-        print(content_page.render_text(cfg))
+        print(cfg.get('site'))
+        print(content_page.render_content(cfg))
